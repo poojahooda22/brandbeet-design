c390c405f4c50765a8b499d7ca7aa0d2c6751c1a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-16 14:56:37 +0530

    first commit

---
+++ src/components/navbar/Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-full h-[10vh] flex items-center justify-between px-[2.5vw] '>
+      <div><h1 className='mix-blend-difference'>Hello</h1></div>
+      <div><h1 className='mix-blend-difference'>Hello</h1></div>
+    </div>
+  )
+}
+
+export default Navbar

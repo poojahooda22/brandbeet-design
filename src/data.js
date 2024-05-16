6b051a64331cfbc93ffacf8402d0337675097c6a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-16 21:54:15 +0530

    added

--- src/data.js
+++ src/data.js
@@ -5,5 +5,8 @@ export const data = [
     {
         image1: "https://assets-global.website-files.com/64b117fb0e09e8ce5768e4fe/64ba4bd8fa4742b8e1f3b6b8_LAYOUT-8-p-1600.jpg",
     },
+    {
+        image1: "https://assets-global.website-files.com/64b117fb0e09e8ce5768e4fe/64ba4bd8fa4742b8e1f3b6b8_LAYOUT-8-p-1600.jpg",
+    },
 
 ]

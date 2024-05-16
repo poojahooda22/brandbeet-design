98cbe2a08559a4ca6f8611be3035e3d757352aeb
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-16 19:42:28 +0530

    updated index

--- src/components/navbar/Index.jsx
+++ src/components/navbar/Index.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import style from './style.module.css'
+
 
 const Navbar = () => {
   return (
-    <div className='w-full h-[10vh] flex items-center justify-between px-[2.5vw] '>
-      <div><h1 className='mix-blend-difference'>Hello</h1></div>
-      <div><h1 className='mix-blend-difference'>Hello</h1></div>
+    <div className='fixed w-full flex items-center justify-between px-[5vw] py-[2vw] sm:px-[2.5vw] cursor-pointer sm:py-[2vw] '>
+        <div className={`lftNav ${style.lftNav} h-[4vh] overflow-hidden text-[4.5vw] leading-[5vw] sm:text-[1.5vw] sm:leading-[1.8vw] tracking-tighter font-semibold`}>
+            <h1 className={`logo ${style.logo}`}>Brandbeet</h1>
+            <h1 className=''>Brandbeet</h1>
+        </div>
+        <div className={`rghtNav ${style.rghtNav} h-[3vh] text-[3vw] leading-[4vw] overflow-hidden sm:text-[1vw] sm:leading-[1.4vw]  tracking-tighter font-semibold`}>
+            <h3 className=''>hello@brandbeet.com</h3>
+            <h3 className=''>hello@brandbeet.com</h3>
+        </div>
     </div>
   )
 }

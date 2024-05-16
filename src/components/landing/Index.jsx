80cd8f82435b42405f54a9bdec6801c5f0b68044
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-16 21:45:20 +0530

    updated index

--- src/components/landing/Index.jsx
+++ src/components/landing/Index.jsx
@@ -8,7 +8,8 @@ function Landing() {
         className='part1 w-[100%] h-[100vh] bg-black '
     >
       <div 
-        className='contentPart1 w-[100%] h-[100vh]'>
+        className='contentPart1 w-[100%] h-[100vh]'
+    >
         <div className='rotateDiv w-[200vw] h-[200vh] bg-black  flex items-center justify-center gap-4'>
             <div className={`rowDiv ${style.rowDiv}`}></div>
             <div className={`rowDiv ${style.rowDiv}`}></div>

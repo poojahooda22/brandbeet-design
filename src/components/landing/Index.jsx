3b5cfd524766b37768e7e6e98a1a2807bbf7a17d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-16 21:29:12 +0530

    updated landing

--- src/components/landing/Index.jsx
+++ src/components/landing/Index.jsx
@@ -4,10 +4,14 @@ import style from './style.module.css'
 
 function Landing() {
   return (
-    <div className='part1 w-full h-[100vh] bg-black '>
-      <div className='contentPart1 w-full h-[100vh] flex items-center justify-center'>
-        <div className='rotateDiv w-[200%] h-[200vh] bg-black'>
-            <div className={`rowDiv ${style.rowDiv} flex items-center justify-center`}></div>
+    <div className='part1 w-[100%] h-[100vh] bg-black '>
+      <div className='contentPart1 w-[100%] h-[100vh]'>
+        <div className='rotateDiv w-[200vw] h-[200vh] bg-black  flex items-center justify-center gap-4'>
+            <div className={`rowDiv ${style.rowDiv}`}></div>
+            <div className={`rowDiv ${style.rowDiv}`}></div>
+            <div className={`rowDiv ${style.rowDiv}`}></div>
+            <div className={`rowDiv ${style.rowDiv}`}></div>
+            <div className={`rowDiv ${style.rowDiv}`}></div>
         </div>
       </div>
     </div>

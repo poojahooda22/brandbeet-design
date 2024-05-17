214156b8e0ed2d5e5599590efc881d8807dfba63
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-17 23:02:57 +0530

    added

--- src/data.js
+++ src/data.js
@@ -24,7 +24,7 @@ export const data = [
         image8: "https://assets-global.website-files.com/64b117fb0e09e8ce5768e4fe/64ba4bd95bf2f4fe4cfdf9b9_LAYOUT-4-p-1600.jpg",
     },
     {
-        image1: "https://assets-global.website-files.com/64b117fb0e09e8ce5768e4fe/64ba4bd9e3fee0b8d4e3a73c_LAYOUT-6-p-1600.jpg",
+        image9: "https://assets-global.website-files.com/64b117fb0e09e8ce5768e4fe/64ba4bd9e3fee0b8d4e3a73c_LAYOUT-6-p-1600.jpg",
     },
     {
         image1: "https://assets-global.website-files.com/64b117fb0e09e8ce5768e4fe/64ba4bd82d6d25df67e7d4b8_LAYOUT-3-p-1600.jpg",

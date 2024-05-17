fa988e677b8ccf8237c287cf78ccc0e836d6aa2e
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-17 23:13:00 +0530

    added

--- src/components/landing/Index.jsx
+++ src/components/landing/Index.jsx
@@ -23,7 +23,7 @@ function Landing() {
           <div className={`rowDiv ${style.rowDiv} space-y-8`}>
             <div className={`imgDiv ${style.imgDiv} bg-white`}>
 
-            </div>
+ </div>
           </div>
           <div className={`rowDiv ${style.rowDiv}`}> </div>
           <div className={`rowDiv ${style.rowDiv}`}> </div>

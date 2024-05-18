1284a282b025002e9730823890ba5f68b5513a83
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-18 16:10:57 +0530

    updated landing

--- src/components/landing/Index.jsx
+++ src/components/landing/Index.jsx
@@ -3,6 +3,10 @@ import style from './style.module.css'
 import { data } from '@/data'
 import Image from 'next/image'
 import { useGSAP } from '@gsap/react'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function Landing() {
 
@@ -11,11 +15,30 @@ function Landing() {
       scrollTrigger: {
         trigger: '.part1',
         start: '50% 50%',
-
+        end: '150% 50%',
         markers: true,
+        scrub: 1,
       }
     })
+    tl.to(".rotateDiv", {
+      rotate: -15,
+      scale: 0.8
+    }, 'a')
+    tl.to('.rowDiv2', {
+      marginTop: "-3%"
+    }, 'a')
+    tl.to('.rowDiv3', {
+      marginTop: "-5%"
+    }, 'a')
+    tl.to('.rowDiv4', {
+      marginTop: "-8%"
+    }, 'a')
+    tl.to('.rowDiv5', {
+      marginTop: "-10%"
+    }, 'a')
   })
+
+
   return (
     <div 
       className='part1 sm:w-[100%] 
@@ -29,12 +52,12 @@ function Landing() {
       >
         <div 
           className='rotateDiv w-[200vw] 
-          h-[200vh] bg-black  
+          h-[200vh] bg-black  scale-100
           flex items-center
           justify-center gap-[2vw]'
         >
           {/* // 1st row */}
-          <div className={`rowDiv ${style.rowDiv} rowDiv1 mt-[-3%] flex flex-col gap-[2vw] `}>
+          <div className={`rowDiv ${style.rowDiv} rowDiv1 flex flex-col gap-[2vw] `}>
             <div className={`imgDiv ${style.imgDiv} `}>
               <img className='w-[100%] h-[100%] object-cover' src="https://assets-global.website-files.com/64b117fb0e09e8ce5768e4fe/64ba4bd85edf60c6b4da45d7_LAYOUT-19-p-1600.jpg" alt="" />
             </div>
@@ -49,7 +72,7 @@ function Landing() {
             </div>  
           </div>
           {/* // 2nd row */}
-          <div className={`rowDiv ${style.rowDiv} rowDiv2 mt-[-5%] flex flex-col gap-[2vw] `}>
+          <div className={`rowDiv ${style.rowDiv} rowDiv2 mt-[-.4%] flex flex-col gap-[2vw] `}>
             <div className={`imgDiv ${style.imgDiv} `}>
               <img className='w-[100%] h-[100%] object-cover' src="https://assets-global.website-files.com/64b117fb0e09e8ce5768e4fe/64ba4bd8f0d3c57c3311f16d_LAYOUT-1-p-500.jpg" alt="" />
             </div>
@@ -64,7 +87,7 @@ function Landing() {
             </div>
           </div>
           {/* // 3rd row */}
-          <div className={`rowDiv ${style.rowDiv} rowDiv3 mt-[-8%] flex flex-col gap-[2vw] `}> 
+          <div className={`rowDiv ${style.rowDiv} rowDiv3 mt-[-4%] flex flex-col gap-[2vw] `}> 
             <div className={`imgDiv ${style.imgDiv} `}>
               <img className='w-[100%] h-[100%] object-cover' src="https://assets-global.website-files.com/64b117fb0e09e8ce5768e4fe/64ba4bd8207f66103ea6893d_LAYOUT-2-p-1600.jpg" alt="" />
             </div>
@@ -79,7 +102,7 @@ function Landing() {
             </div>
           </div>
           {/* // 4th row */}
-          <div className={`rowDiv ${style.rowDiv} rowDiv4 mt-[-12%] flex flex-col gap-[2vw] `}>
+          <div className={`rowDiv ${style.rowDiv} rowDiv4 mt-[-6%] flex flex-col gap-[2vw] `}>
             <div className={`imgDiv ${style.imgDiv} `}>
               <img className='w-[100%] h-[100%] object-cover' src="https://assets-global.website-files.com/64b117fb0e09e8ce5768e4fe/64ba4bd8fa4742b8e1f3b6b8_LAYOUT-8-p-1600.jpg" alt="" />
             </div>
@@ -94,7 +117,7 @@ function Landing() {
             </div>
           </div>
           {/* // 5th row */}
-          <div className={`rowDiv ${style.rowDiv} rowDiv5 mt-[-15%] flex flex-col gap-[2vw] `}> 
+          <div className={`rowDiv ${style.rowDiv} rowDiv5 mt-[-8%] flex flex-col gap-[2vw] `}> 
             <div className={`imgDiv ${style.imgDiv} `}>
               <img className='w-[100%] h-[100%] object-cover' src="https://assets-global.website-files.com/64b117fb0e09e8ce5768e4fe/64ba4bd84ea0e4d87b20d630_LAYOUT-16-p-1600.jpg" alt="" />
             </div>

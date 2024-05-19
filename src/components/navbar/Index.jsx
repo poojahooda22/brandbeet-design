3111aadddd6506074c6a466e68942f277649f0ba
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-19 18:58:34 +0530

    updated index

--- src/components/navbar/Index.jsx
+++ src/components/navbar/Index.jsx
@@ -5,11 +5,11 @@ import style from './style.module.css'
 const Navbar = () => {
   return (
     <div className='fixed w-full flex items-center justify-between px-[5vw] py-[4vw] sm:px-[2.5vw] cursor-pointer sm:py-[2vw] mix-blend-difference z-[2] '>
-        <div className={`lftNav ${style.lftNav} h-[3.5vh]  sm:h-[4vh] overflow-hidden text-[4.5vw] leading-[5vw] sm:text-[1.5vw] sm:leading-[1.8vw] tracking-tighter text-[#fff]`}>
+        <div className={`lftNav ${style.lftNav} h-[4.5vw] sm:h-[2vw]  overflow-hidden text-[4.5vw] leading-[5vw] sm:text-[1.5vw] sm:leading-[1.8vw] tracking-tighter text-[#fff]`}>
             <h1 className={`logo ${style.logo}`}>Brandbeet</h1>
             <h1 className=''>Brandbeet</h1>
         </div>
-        <div className={`rghtNav ${style.rghtNav} h-[3.2vh]  sm:h-[3vh] text-[3vw] leading-[4vw] overflow-hidden md:text-[1vw] sm:leading-[1.4vw]  tracking-tighter text-[#fff]`}>
+        <div className={`rghtNav ${style.rghtNav} h-[3.5vw] sm:h-[1.5vw] text-[3vw] leading-[4vw] overflow-hidden sm:text-[1vw] sm:leading-[1.4vw]  tracking-tight text-[#fff]`}>
             <h3 className=''>hello@brandbeet.com</h3>
             <h3 className=''>hello@brandbeet.com</h3>
         </div>

24d86098ed1dde6cbda6da1d055360a88db51a93
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-19 23:00:55 +0530

    updated index

--- src/components/about/Index.jsx
+++ src/components/about/Index.jsx
@@ -11,12 +11,15 @@ function About() {
         const tl = gsap.timeline({
             scrollTrigger: {
               trigger: '.part2',
-              start: '50% 50%',
-              end: '250% 50%',
+              start: '0% 70%',
+              end: '50% 50%',
               markers: true,
               scrub: 1,
-              pin: true,
             }
+        })
+        tl.to('.roundedDivWrapper', {
+            height: 0,
+            marginTop: 0,
         })
     })
   return (
@@ -24,7 +27,7 @@ function About() {
         
         {/* //rounded div */}
         <div className='roundedDivWrapper relative w-[100%] h-[100px]  overflow-hidden mt-[-5vw] '>
-            <div className='roundedDiv w-[150%] h-[750%] rounded-[50%]  absolute left-1/2 -translate-x-1/2 '></div>
+            <div className='roundedDiv w-[150%] h-[750%] rounded-[50%] bg-[#fff]  absolute left-1/2 -translate-x-1/2 '></div>
         </div>
         {/* <div className='w-[100%] flex items-center justify-start px-[5vw] sm:px-[2.5vw]'>
             <div className='text-[10vw] leading-[13vw] sm:text-[7vw] sm:leading-[9vw] tracking-tighter sm:whitespace-nowrap text-[#e2e2e2]'>

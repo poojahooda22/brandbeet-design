5023b901d56fc1c23c7f008906246b6ee0b576ca
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-19 21:48:13 +0530

    updated about

--- src/components/about/Index.jsx
+++ src/components/about/Index.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { useGSAP } from '@gsap/react'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function About() {
+
+    useGSAP(() => {
+        const tl = gsap.timeline({
+            scrollTrigger: {
+              trigger: '.part2',
+              start: '50% 50%',
+              end: '250% 50%',
+              markers: true,
+              scrub: 1,
+              pin: true,
+            }
+        })
+    })
   return (
     <div className='part2 w-[100%] h-[100vh] bg-[#fff] '>
         
         {/* //rounded div */}
-        <div className='roundedDivWrapper relative w-[100%] h-[100px] bg-green-400 overflow-hidden'>
-            <div className='roundedDiv w-[150%] h-[750%] rounded-[50%] bg-[red] absolute left-1/2 -translate-x-1/2'></div>
+        <div className='roundedDivWrapper relative w-[100%] h-[100px]  overflow-hidden mt-[-5vw] '>
+            <div className='roundedDiv w-[150%] h-[750%] rounded-[50%]  absolute left-1/2 -translate-x-1/2 '></div>
         </div>
         {/* <div className='w-[100%] flex items-center justify-start px-[5vw] sm:px-[2.5vw]'>
             <div className='text-[10vw] leading-[13vw] sm:text-[7vw] sm:leading-[9vw] tracking-tighter sm:whitespace-nowrap text-[#e2e2e2]'>

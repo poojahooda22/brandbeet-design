6f428144cd2f42597873d63e9a9558d2f6ec9f41
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-19 20:40:41 +0530

    updated index

--- src/components/about/Index.jsx
+++ src/components/about/Index.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='part2 w-[100%] h-[100vh] bg-[#fff] flex items-center justify-start px-[5vw] sm:px-[2.5vw]'>
-
-        <div className='roundedDivWrapper '>
-            <div className='rounded-div'></div>
+    <div className='part2 w-[100%] h-[100vh] bg-[#fff] '>
+        
+        {/* //rounded div */}
+        <div className='roundedDivWrapper w-[100%] h-[100px] bg-green-400 '>
+            <div className='rounded-div w-[150%] h-[750%]'></div>
         </div>
-        <div className='text-[10vw] leading-[13vw] sm:text-[7vw] sm:leading-[9vw] tracking-tighter sm:whitespace-nowrap text-[#e2e2e2]'>
-            <h2>Strategic design solutions</h2>
-            <h2 className='sm:ml-[14vw]'>the fuel your bottom line.</h2>
-        </div>
+        {/* <div className='w-[100%] flex items-center justify-start px-[5vw] sm:px-[2.5vw]'>
+            <div className='text-[10vw] leading-[13vw] sm:text-[7vw] sm:leading-[9vw] tracking-tighter sm:whitespace-nowrap text-[#e2e2e2]'>
+                <h2>Strategic design solutions</h2>
+                <h2 className='sm:ml-[14vw]'>the fuel your bottom line.</h2>
+            </div>
+        </div> */}
+        
     </div>
   )
 }

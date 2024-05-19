48bce8101205e5867bbd57c3a232d17cf105bf03
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-19 21:39:38 +0530

    updated index

--- src/components/about/Index.jsx
+++ src/components/about/Index.jsx
@@ -5,8 +5,8 @@ function About() {
     <div className='part2 w-[100%] h-[100vh] bg-[#fff] '>
         
         {/* //rounded div */}
-        <div className='roundedDivWrapper w-[100%] h-[100px] bg-green-400 '>
-            <div className='rounded-div w-[150%] h-[750%] rounded-[50%] bg-[red]'></div>
+        <div className='roundedDivWrapper relative w-[100%] h-[100px] bg-green-400 overflow-hidden'>
+            <div className='roundedDiv w-[150%] h-[750%] rounded-[50%] bg-[red] absolute left-1/2 -translate-x-1/2'></div>
         </div>
         {/* <div className='w-[100%] flex items-center justify-start px-[5vw] sm:px-[2.5vw]'>
             <div className='text-[10vw] leading-[13vw] sm:text-[7vw] sm:leading-[9vw] tracking-tighter sm:whitespace-nowrap text-[#e2e2e2]'>

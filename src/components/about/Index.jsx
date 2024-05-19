42dcb4b56f8c03501f29f04ad80d9eefd2fffa81
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-19 19:13:24 +0530

    updated index

--- src/components/about/Index.jsx
+++ src/components/about/Index.jsx
@@ -4,8 +4,8 @@ function About() {
   return (
     <div className='part2 w-[100%] h-[100vh] bg-[#fff] flex items-center justify-start px-[5vw] sm:px-[2.5vw]'>
 
-        <div className='roundedDivWrapper'>
-
+        <div className='roundedDivWrapper '>
+            <div className='rounded-div'></div>
         </div>
         <div className='text-[10vw] leading-[13vw] sm:text-[7vw] sm:leading-[9vw] tracking-tighter sm:whitespace-nowrap text-[#e2e2e2]'>
             <h2>Strategic design solutions</h2>

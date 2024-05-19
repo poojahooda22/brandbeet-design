5b59005519582466ec6dc1db1a997672a4f38ef3
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-19 18:58:15 +0530

    created index

---
+++ src/components/about/Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div className='part2 w-[100%] h-[100vh] bg-[#fff]'>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default About

de72c061596f37bae216e56e863b14c641df7a68
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-19 21:04:40 +0530

    updated index

--- src/components/about/Index.jsx
+++ src/components/about/Index.jsx
@@ -6,7 +6,7 @@ function About() {
         
         {/* //rounded div */}
         <div className='roundedDivWrapper w-[100%] h-[100px] bg-green-400 '>
-            <div className='rounded-div w-[150%] h-[750%]'></div>
+            <div className='rounded-div w-[150%] h-[750%] rounded-[50%] bg-[red]'></div>
         </div>
         {/* <div className='w-[100%] flex items-center justify-start px-[5vw] sm:px-[2.5vw]'>
             <div className='text-[10vw] leading-[13vw] sm:text-[7vw] sm:leading-[9vw] tracking-tighter sm:whitespace-nowrap text-[#e2e2e2]'>

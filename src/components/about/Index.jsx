58584eb3aa8020c7fd0197fae462113602f825af
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-19 19:09:41 +0530

    updated index

--- src/components/about/Index.jsx
+++ src/components/about/Index.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='part2 w-[100%] h-[100vh] bg-[#fff]'>
-      <div>
-
+    <div className='part2 w-[100%] h-[100vh] bg-[#fff] flex items-center justify-start px-[5vw] sm:px-[2.5vw]'>
+      <div className='text-[10vw] leading-[13vw] sm:text-[7vw] sm:leading-[9vw] tracking-tighter sm:whitespace-nowrap text-[#e2e2e2]'>
+        <h2>Strategic design solutions</h2>
+        <h2 className='sm:ml-[14vw]'>the fuel your bottom line.</h2>
       </div>
     </div>
   )

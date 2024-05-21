a823b8ed47cf58e805c3c3cd9bf42021f999373a
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-21 20:08:21 +0530

    updated index

--- src/components/about/Index.jsx
+++ src/components/about/Index.jsx
@@ -15,6 +15,32 @@ gsap.registerPlugin(ScrollTrigger)
 function About() {
 
     useGSAP(() => {
+        let mm = gsap.matchMedia();
+
+        mm.add("(min-width: 768px", () => {
+            gsap.to('.textAreaHover h2', {
+                width: '100%',
+                duration: 1,
+                scrollTrigger: {
+                    trigger: '.content2',
+                    start: '50% 80%',
+                    end: '50% 50%',
+                    scrub: 1,
+                }
+            })
+            gsap.to('.textAreaHover2 h2', {
+                width: '100%',
+                delay: 2,
+                duration: 2,
+                scrollTrigger: {
+                    trigger: '.content2',
+                    start: '30% 50%',
+                    end: '50% 50%',
+                    scrub: 1,
+                }
+            })
+        })
+
         const tl = gsap.timeline({
             scrollTrigger: {
               trigger: '.part2',
@@ -28,27 +54,7 @@ function About() {
             marginTop: 0,
         })
 
-        gsap.to('.textAreaHover h2', {
-            width: '100%',
-            duration: 1,
-            scrollTrigger: {
-                trigger: '.content2',
-                start: '50% 80%',
-                end: '50% 50%',
-                scrub: 1,
-            }
-        })
-        gsap.to('.textAreaHover2 h2', {
-            width: '100%',
-            delay: 2,
-            duration: 2,
-            scrollTrigger: {
-                trigger: '.content2',
-                start: '30% 50%',
-                end: '50% 50%',
-                scrub: 1,
-            }
-        })
+        
     })
   return (
     <div className='part2 w-[100%] bg-secondary '>
@@ -57,22 +63,22 @@ function About() {
         <div className='hidden sm:flex roundedDivWrapper relative w-[100%] h-[100px]  overflow-hidden mt-[-5vw] '>
             <div className='roundedDiv w-[150%] h-[750%] rounded-[50%] bg-secondary  absolute left-1/2 -translate-x-1/2 '></div>
         </div>
-        <div className='content2 w-[100%] h-[100vh] relative flex items-center px-[5vw] sm:px-[2.5vw] overflow-hidden'>
+        <div className='content2 w-[100%] h-[50vh] sm:h-[100vh] relative flex items-center px-[5vw] sm:px-[2.5vw] overflow-hidden'>
             <div className='textArea absolute left-[5%] sm:left-[2%] opacity-[.2] text-[10vw] leading-[10vw]  sm:text-[7vw] tracking-tighter sm:whitespace-nowrap '>
                 <h2>Strategic design solutions</h2>
             </div>
             <div className='textAreaHover absolute left-[5%] sm:left-[2%] opacity-100 text-[10vw] leading-[10vw] sm:text-[7vw]  tracking-tighter sm:whitespace-nowrap '>
-                <h2 className=' overflow-hidden w-[0%]'>Strategic design solutions</h2>
+                <h2 className=' overflow-hidden w-[100%] sm:w-[0%]'>Strategic design solutions</h2>
             </div>
             <div className='textArea absolute sm:left-[10%] left-[5%] mt-[40vw] sm:mt-[18vw]  opacity-[.2] leading-[10vw] text-[10vw]  sm:text-[7vw] tracking-tighter sm:whitespace-nowrap'>
                 <h2>that fuel your bottom line.</h2>
             </div>
             <div className='textAreaHover2  absolute left-[5%] sm:left-[10%] mt-[40vw] sm:mt-[18vw] leading-[10vw]  opacity-100 text-[10vw]  sm:text-[7vw] tracking-tighter sm:whitespace-nowrap'>
-                <h2 className='overflow-hidden w-[0%]'>that fuel your bottom line.</h2>
+                <h2 className='overflow-hidden w-[100%] sm:w-[0%]'>that fuel your bottom line.</h2>
             </div>
         </div>
 
-        <div className=' px-[5vw] sm:px-[2.5vw] sm:flex items-center justify-between pb-[12vw] sm:pb-[4vw] '>
+        <div className=' px-[5vw] sm:px-[2.5vw] sm:flex items-center justify-between pb-[12vw] sm:pb-[4vw] pt-[14vw]'>
             <h3 className='text-[4vw] sm:text-[1.2vw] text-textComplimentary tracking-tight mb-[4vw] sm:mb-0'>Unlock profit with monthly design sprints.</h3>
             <div className='cta flex items-center justify-between sm:gap-[1vw] cursor-pointer'>
                 <div className='bg-primary px-[10vw] py-[6vw] sm:px-[2.2vw] sm:py-[1.2vw] rounded-full'>

09a1f7427e8138338b2f54052abf5aebf02c8fc1
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-21 20:08:44 +0530

    created index

---
+++ src/components/mywork/Index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Work() {
+  return (
+    <div className='w-full h-[100vh] bg-primary'>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Work

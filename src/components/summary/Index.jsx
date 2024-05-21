464e92cf4635b33016de3bbe864046bf29b40aca
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-21 20:11:25 +0530

    update

--- src/components/summary/Index.jsx
+++ src/components/summary/Index.jsx
@@ -7,38 +7,44 @@ gsap.registerPlugin(ScrollTrigger)
 
 
 function Summary() {
-
+  
   useGSAP(() => {
-    const tl = gsap.timeline({
-      scrollTrigger: {
-        trigger: '.part4Sum',
-        start: '50% 100%',
-        end: '50% 50%',
-        scrub: 1, 
-      }
-    })
+    // const tl = gsap.timeline({
+    //   scrollTrigger: {
+    //     trigger: '.part4Sum',
+    //     start: '50% 100%',
+    //     end: '50% 50%',
+    //     scrub: 1, 
+    //   }
+    // })
     
-    gsap.to('.textAreaHover2 h2', {
-      width: '100%',
-      duration: 1,
-      scrollTrigger: {
-        trigger: '.part4Sum',
-        start: '50% 70%',
-        end: '50% 50%',
-        scrub: 1,
-      }
+    let mm = gsap.matchMedia();
+
+    mm.add("(min-width: 768px", () => {
+      gsap.to('.textAreaHover2 h2', {
+        width: '100%',
+        duration: 1,
+        scrollTrigger: {
+          trigger: '.part4Sum',
+          start: '50% 70%',
+          end: '50% 50%',
+          scrub: 1,
+        }
+      })
+      gsap.to('.textAreaHover3 h2', {
+        width: '100%',
+        delay: 2,
+        duration: 2,
+        scrollTrigger: {
+          trigger: '.part4Sum',
+          start: '30% 40%',
+          end: '50% 50%',
+          scrub: 1,
+        }
+      })
     })
-    gsap.to('.textAreaHover3 h2', {
-      width: '100%',
-      delay: 2,
-      duration: 2,
-      scrollTrigger: {
-        trigger: '.part4Sum',
-        start: '30% 40%',
-        end: '50% 50%',
-        scrub: 1,
-      }
-    })
+
+    
   })
 
 
@@ -51,13 +57,13 @@ function Summary() {
             <h2 className='text-center'>A dedicated team at</h2>
           </div>
           <div className='textAreaHover2 absolute left-[7%] sm:left-[20%] opacity-100 text-[10vw] leading-[10vw] sm:text-[7vw]  tracking-tighter sm:whitespace-nowrap '>
-            <h2 className=' overflow-hidden w-[0%]'>A dedicated team at</h2>
+            <h2 className=' overflow-hidden w-[100%] sm:w-[0%]'>A dedicated team at</h2>
           </div>
           <div className='textArea absolute sm:left-[30%] left-[18%] mt-[12vw] sm:mt-[10vw]  opacity-[.2] leading-[10vw] text-[10vw]  sm:text-[7vw] tracking-tighter sm:whitespace-nowrap'>
             <h2 className='text-center'>your disposal.</h2>
           </div>
           <div className='textAreaHover3  absolute left-[18%] sm:left-[30%] mt-[12vw] sm:mt-[10vw] leading-[10vw]  opacity-100 text-[10vw]  sm:text-[7vw] tracking-tighter sm:whitespace-nowrap'>
-            <h2 className='overflow-hidden w-[0%] '>your disposal.</h2>
+            <h2 className='overflow-hidden w-[100%] sm:w-[0%] '>your disposal.</h2>
           </div>
         </div>     
       </div> 

a33ef219eaf3bd86722af23d503ec395ce9188d5
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-22 12:00:32 +0530

    updated index

--- src/components/mywork/Index.jsx
+++ src/components/mywork/Index.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 function Work() {
   return (
-    <div className='w-full h-[100vh] bg-primary'>
-      <div>
-
+    <div className='part7 w-full h-[100vh] bg-primary text-secondary'>
+      <div className='ourWorktxt'>
+        <h1>Our</h1>
+        <h1>Work</h1>
       </div>
     </div>
   )

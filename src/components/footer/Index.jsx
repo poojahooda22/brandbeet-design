93d5fb9dcca35bd79b2ddb22cf47f24046b46c92
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-22 19:58:18 +0530

    added

--- src/components/footer/Index.jsx
+++ src/components/footer/Index.jsx
@@ -12,7 +12,7 @@ function Footer() {
     //     const tl = gsap.timeline({
     //         scrollTrigger: {
     //           trigger: '.part8',
-    //           start: '50% ',        
+    //           start: ' ',        
     //         }
     //     })
     //     tl.from('.footPtag p', {

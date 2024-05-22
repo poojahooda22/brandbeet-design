9a08493bc38c94ac2f9b1e2ec44d2c85a0789fa7
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-22 12:17:21 +0530

    added

--- src/components/mywork/Index.jsx
+++ src/components/mywork/Index.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 
 function Work() {
   return (
-    <div className='part7 w-full h-[100vh] bg-primary text-secondary'>
-      <div className='ourWorktxt'>
-        <h1>Our</h1>
-        <h1>Work</h1>
+    <div className='part7 w-full h-[100vh] bg-primary text-secondary flex items-center justify-center'>
+      <div className='ourWorktxt relative w-[60vw] h-[10vh] z-[9] '>
+        <h1 className='our absolute top-0 left-[35%] sm:text-[5vw] -translate-x-1/2 -translate-y-1/2' >Our</h1>
+        <h1 className='work absolute bottom-0 right-[35%] sm:text-[5vw] translate-x-1/2 translate-y-1/2' >Work</h1>
+      </div>
+      <div className='ourWorktxtDiv'>
+          <div className='scrollWork'>
+              <div className='scrollWorkImg'>
+
+              </div>
+          </div>
       </div>
     </div>
   )

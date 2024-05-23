a272ec07ce53f6f7ce19d99d19231b1382d666a7
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-23 22:29:19 +0530

    added

--- src/components/footer/Index.jsx
+++ src/components/footer/Index.jsx
@@ -21,8 +21,7 @@ function Footer() {
                                 B
                                 <>r</>
                                 <>a</>
-                                <>n</>
-                                <>d</><>b</><>e</><>e</>t
+                                <>n</><>d</><>b</><>e</><>e</>t
                             </h2>
                             <div className='text-[4vw] sm:text-[1.5vw] text-center tracking-tighter'>
                                 <div className='footPtag overflow-hidden'>

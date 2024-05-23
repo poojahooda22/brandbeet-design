cb397066c924a387366aa33baffb124c42609990
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-23 22:26:48 +0530

    added

--- src/components/footer/Index.jsx
+++ src/components/footer/Index.jsx
@@ -25,7 +25,7 @@ function Footer() {
                                 <>d</>
                                 <>b</>
                                 <>e</>
-                                <span>e</span>
+                                <spa>e</spa>
                                 <span>t</span>
                             </h2> 
                             <div className='text-[4vw] sm:text-[1.5vw] text-center tracking-tighter'>

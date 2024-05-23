af708c046539aa9957d130e5eb98b7e81b6b99d8
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-23 22:24:37 +0530

    added

--- src/components/footer/Index.jsx
+++ src/components/footer/Index.jsx
@@ -20,7 +20,7 @@ function Footer() {
                             <h2 className='text-[12vw] sm:text-[8vw] tracking-tighter'>
                                 B
                                 <>r</>
-                                <spa>a</spa>
+                                <sp>a</sp>
                                 <span>n</span>
                                 <span>d</span>
                                 <span>b</span>

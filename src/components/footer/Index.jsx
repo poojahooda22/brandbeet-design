71720380f118d609721c614c0b75527b9dadb192
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-23 22:29:58 +0530

    added

--- src/components/footer/Index.jsx
+++ src/components/footer/Index.jsx
@@ -18,7 +18,7 @@ function Footer() {
                     <div className='w-full flex flex-col items-center'>
                         <div className=' flex flex-col items-center justify-between'>
                             <h2 className='text-[12vw] sm:text-[8vw] tracking-tighter'>
-                                B<>r</><>a</><>n</><>d</><>b</><>e</><>et
+                                B<>r</><>a</><>n</><>d</><>b</><>e</>et
                             </h2>
                             <div className='text-[4vw] sm:text-[1.5vw] text-center tracking-tighter'>
                                 <div className='footPtag overflow-hidden'>

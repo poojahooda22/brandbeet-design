102f79d27c1b7858d900c2d0b2f12639b3ae20ca
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-27 23:32:23 +0530

    updated index

--- src/components/mywork/Index.jsx
+++ src/components/mywork/Index.jsx
@@ -3,6 +3,7 @@ import { useGSAP } from '@gsap/react'
 import { gsap, Power3 } from 'gsap'
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import style from './style.module.css'
+import Image from 'next/image';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -32,7 +33,8 @@ function Work() {
     }, 'sc')
     tl.to('.scrollWorkImg', {
       marginTop: '-460%',
-      // ease: Power3.easeIn
+      duration: 2,
+      ease: Power3.easeIn
     })   
   })
 
@@ -46,7 +48,8 @@ function Work() {
       <div className='ourWorktxtDiv relative w-[75vw] sm:w-[60vw] flex items-center justify-center h-[0vh] overflow-hidden'>
           <div className='scrollWork w-[100%] h-[60vh] sm:h-[60vh] overflow-hidden'>
               <div className={`scrollWorkImg ${style.scrollWorkImg} w-[100%] mt-[0%]`}>
-                <img className='w-[100%] h-[100%] object-cover' src="https://assets-global.website-files.com/64b117fb0e09e8ce5768e4fe/64ba4bd9fa4742b8e1f3b6f6_LAYOUT-7-p-2000.jpg" alt="" />
+                <Image src="https://assets-global.website-files.com/64b117fb0e09e8ce5768e4fe/64ba4bd9fa4742b8e1f3b6f6_LAYOUT-7-p-2000.jpg" alt="" width={1000} height={100} className='w-[100%] h-[100%] object-cover'/>
+                
                 <img className='w-[100%] h-[100%] object-cover' src="https://assets-global.website-files.com/64b117fb0e09e8ce5768e4fe/64ba4bd8e3fee0b8d4e3a67b_LAYOUT-5-p-1080.jpg" alt="" />
                 <img className='w-[100%] h-[100%] object-cover' src="https://assets-global.website-files.com/64b117fb0e09e8ce5768e4fe/64ba4bd95bf2f4fe4cfdf9b9_LAYOUT-4-p-1600.jpg" alt="" />
                 <img className='w-[100%] h-[100%] object-cover' src="https://assets-global.website-files.com/64b117fb0e09e8ce5768e4fe/64be4044e5242bb154e2c347_HF%204-p-1600.jpg" alt="" />

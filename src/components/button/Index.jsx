241b4bd8cd9be1fc92efb19a41a3949008341819
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-27 23:22:11 +0530

    updated

--- src/components/button/Index.jsx
+++ src/components/button/Index.jsx
@@ -3,7 +3,7 @@ import style from './style.module.css'
 
 function Button({color, text, color2}) {
   return (
-    <div className={`${color} px-[10vw] py-[6vw] sm:px-[2.2vw] sm:py-[1.2vw] rounded-full`}>
+    <div className={`${color} px-[10vw] py-[6vw] sm:px-[2.2vw] sm:py-[1.2vw] rounded-full cursor-pointer`}>
         <div 
             className={`ctaBook ${style.ctaBook} text-[4vw] sm:text-[1vw] tracking-tight
                 ${color2} h-[5.2vw] sm:h-[1.4vw] whitespace-nowrap overflow-hidden`}
